bf0f03e280340dc5d4a87d550ef8a1996af623fc
aelmardhi <aelmardhi@gmail.com>
2022-09-04 21:06:12 +0200

    clear msg queue

--- src/TrafficLight.cpp
+++ src/TrafficLight.cpp
@@ -26,6 +26,7 @@ void MessageQueue<T>::send(T &&msg)
     // FP.4a : The method send should use the mechanisms std::lock_guard<std::mutex> 
     // as well as _condition.notify_one() to add a new message to the queue and afterwards send a notification.
     std::lock_guard<std::mutex> guard (_mutex);
+    _queue.clear(); // clear old messages as soon a new one is ready , the only last message should be seen
     _queue.push_back(std::move(msg));
     _condition.notify_one();
 }
